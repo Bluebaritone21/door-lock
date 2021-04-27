696373be97cae873caaca29ad9f3d7f4cd819fef
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2021-04-27 15:21:04 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,49 @@
+input.onButtonPressed(Button.A, function () {
+    if (Inputs == "") {
+        Inputs = "A"
+        basic.showString(Inputs)
+        basic.clearScreen()
+    } else {
+        Inputs = "" + Inputs + "A"
+        basic.showString(Inputs)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Inputs == "ABBAB") {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . # . # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # . . .
+            # . # . .
+            # . # . .
+            . . # # #
+            . . # # #
+            `)
+    } else {
+        control.reset()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (Inputs == "") {
+        Inputs = "B"
+        basic.showString(Inputs)
+        basic.clearScreen()
+    } else {
+        Inputs = "" + Inputs + "B"
+        basic.showString(Inputs)
+    }
+})
+let Inputs = ""
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    . # . # .
+    . # # # .
+    . # # # .
+    `)
+basic.clearScreen()
